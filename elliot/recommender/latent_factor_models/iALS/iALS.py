958d0d896560fd178661a2c8d71e1395f423e148
salvatore-bufi <bufi.salvatore.94@gmail.com>
2024-07-31 16:09:16 +0200

    try: - starting to save matrix

--- elliot/recommender/latent_factor_models/iALS/iALS.py
+++ elliot/recommender/latent_factor_models/iALS/iALS.py
@@ -15,7 +15,7 @@ from elliot.utils.write import store_recommendation
 from elliot.recommender.latent_factor_models.iALS.iALS_model import iALSModel
 from elliot.recommender.base_recommender_model import BaseRecommenderModel
 from elliot.recommender.base_recommender_model import init_charger
-
+import os
 
 class iALS(RecMixin, BaseRecommenderModel):
     r"""
@@ -98,4 +98,40 @@ class iALS(RecMixin, BaseRecommenderModel):
             print("Iteration Finished")
 
             self.evaluate(it)
+
+    def evaluate(self, it=None, loss=0):
+        if (it is None) or (not (it + 1) % self._validation_rate):
+            recs = self.get_recommendations(self.evaluator.get_needed_recommendations()) # cotniene per ogni utente solo le top-K
+            result_dict = self.evaluator.eval(recs)
+
+            self._losses.append(loss)
+
+            self._results.append(result_dict)
+
+            if it is not None:
+                self.logger.info(f'Epoch {(it + 1)}/{self._epochs} loss {loss/(it + 1):.5f}')
+            else:
+                self.logger.info(f'Finished')
+
+            if self._save_recs:
+                self.logger.info(f"Writing recommendations at: {self._config.path_output_rec_result}")
+                if it is not None:
+                    store_recommendation(recs[1], os.path.abspath(
+                        os.sep.join([self._config.path_output_rec_result, f"{self.name}_it={it + 1}.tsv"])))
+                else:
+                    store_recommendation(recs[1], os.path.abspath(
+                        os.sep.join([self._config.path_output_rec_result, f"{self.name}.tsv"])))
+
+            if (len(self._results) - 1) == self.get_best_arg():
+                # ogni volta che entro qua sovrascrivo matrice score
+
+                if it is not None:
+                    self._params.best_iteration = it + 1
+                self.logger.info("******************************************")
+                self.best_metric_value = self._results[-1][self._validation_k]["val_results"][self._validation_metric]
+                if self._save_weights:
+                    if hasattr(self, "_model"):
+                        self._model.save_weights(self._saving_filepath)
+                    else:
+                        self.logger.warning("Saving weights FAILED. No model to save.")
 
